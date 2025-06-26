2af925b46a53d2d5794c7462ce531826b86f606e
jignesh@1001 <jigneshgurav12@gmail.com>
2025-06-26 17:23:52 +0530

    changes in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,7 @@ export default function About() {
     >
       <h2 className="text-2xl font-semibold mb-4">About Me</h2>
       <p>
-        I’m a full-stack dev with 1yr of product experience and 3yrs freelance.
-        I build scalable frontends with great UX and collaborating closely with
-        core teams.
+       I'm a Full Stack Developer with 1 year of experience building web applications using modern technologies like React, Node.js, and MongoDB. I enjoy working on both frontend and backend, creating user-friendly interfaces and robust APIs. I'm always eager to learn and take on new challenges to grow as a developer.
       </p>
     </motion.section>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
       {/* <img src="/avatar.jpg" alt="Rohan" className="w-32 h-32 rounded-full mx-auto border border gray-200" /> */}
       <h1 className="mt-4 text-4xl font-bold">Jignesh Gurav</h1>
       <p className="mt-2 text-xl text-gray-600">Full‑stack developer building clean, fast & user-first web apps.</p>
-      <p className="mt-1 text-gray-500">Node.js · React · Tailwind CSS</p>
+      <p className="mt-1 text-gray-500">Mumbai, India</p>
     </motion.section>
   );
 }
