4a2a486711d05812c55068ec78e871e6f877fcf0
jignesh@1001 <jigneshgurav12@gmail.com>
2025-07-21 19:20:00 +0530

    text animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,45 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
+
 export default function Hero() {
   return (
-    <motion.section 
-    id="hero"
+    <motion.section
+      id="hero"
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
-    className="py-10 ">
-      {/* <img src="/avatar.jpg" alt="Rohan" className="w-32 h-32 rounded-full mx-auto border border gray-200" /> */}
-      <h1 className="mt-4 text-xl ">Jignesh Gurav</h1>
-      <p className="mt-1  text-sm text-gray-500">Mumbai, India</p>
-      <p className="mt-2 text-l text-gray-600">Full‑stack developer building clean, fast & user-first web apps.
-         With 1 year of experience in designing and developing web applications using modern 
-        technologies. Skilled in both frontend and backend development.  </p>
+      className="py-10 "
+    >
+      {/* Typing effect on H1 */}
+      <TypeAnimation
+        sequence={[
+          "Jignesh Gurav", // text to type
+          10000, // wait 1s
+          // erase
+          // wait 0.5s
+        ]}
+        wrapper="h1"
+        cursor={false}
+        className="mt-4 text-xl"
+      />
+      <TypeAnimation
+        sequence={[
+          "Mumbai, India", // text to type
+          10000, // wait 1s
+          // erase
+          // wait 0.5s
+        ]}
+        wrapper="p"
+        cursor={false}
+        className="mt-1 text-sm text-gray-500"
+      />
+
+      {/* <p className="mt-1 text-sm text-gray-500">Mumbai, India</p> */}
+      <p className="mt-2 text-l text-gray-600">
+        Full‑stack developer building clean, fast & user-first web apps. With 1
+        year of experience in designing and developing web applications using
+        modern technologies. Skilled in both frontend and backend development.
+      </p>
     </motion.section>
   );
 }
